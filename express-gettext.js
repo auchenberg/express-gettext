babd3fc21a9ec51dd058d00ae6f7dc32c3fcd533
Kenneth Auchenberg <kenneth@auchenberg.dk>
2014-10-29 22:20:46 -0700

    Revert "Fixed typo. "
    This reverts commit 71729f4c63781ba7dacf0ffb08b2738989e19b57.

--- express-gettext.js
+++ express-gettext.js
@@ -161,7 +161,7 @@ function isExpressApp(app) {
 function getText(textKey, locale) {
 
     var app = getExpressApp(this);
-    var gt = app.get('gettext instance');
+    var gt = app.set('gettext instance');
 
     var targetLocaleKey = getLocaleKey(locale || this.getLocale());
     var text = gt._getTranslation(targetLocaleKey, textKey);
